678db7e5f5bbde88c16091ffbc10e08f5462f536
Alexei Vinnichek <alexei.vinnichek@mail.ru>
2023-02-22 22:29:44 +0300

    full validation of login form

--- validationLoginForm/validatorLoginForm.js
+++ validationLoginForm/validatorLoginForm.js
@@ -1,27 +1,41 @@
-let form = document.getElementById("loginForm");
-let validateBtn = document.querySelector(".log_btn");
-let inputLogin = document.getElementById('login');
-let reEmail = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i; 
-let reTelephone = /^[\d\+][\d\(\)\- ]{4,14}$/;
-let valueEmail = reEmail.test(inputLogin.value);
-let valueTelephone = reTelephone.test(inputLogin.value);
-let value = valueEmail || valueTelephone;
+const form = document.getElementById("loginForm");
+const inputLogin = document.getElementById('login');
+const inputPassword = document.getElementById("password");
+const reEmail = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i; 
+const reTelephone = /^[\d\+][\d\(\)\- ]{4,14}$/;
+const rePassword = /^[A-Za-z\d]{8,}$/;
 
 if (form) {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("clicked on submit");
-    console.log("inputLogin: ", inputLogin.value);
-    console.log("valueEmail: ", valueEmail);
-    console.log("valueTelephone: ", valueTelephone);
-    if (value) {
-      console.log("Log is correct");
+
+    //Checking login validity
+    const login = inputLogin.value;
+    const isEmailValid = reEmail.test(login);
+    const isTelephoneValid = reTelephone.test(login);
+    const isLoginValid = isEmailValid || isTelephoneValid;
+
+    if (isLoginValid) {
+      console.log("Login pattern is correct");
     } else{
-      console.log("login is incorrect");
-      inputLogin.setCustomValidity("Incorrect!!!!!!");
+      console.log("login pattern is incorrect");
+      inputLogin.setCustomValidity("Please, enter your email or telephone in correct way\n" + 
+                                   "For example: ivanIvanov@mail.com or 378553695847");                       
+    }
+
+    //Checking password validity
+    const password = inputPassword.value;
+    const isPasswordValid = rePassword.test(password);
+    
+    if (isPasswordValid) {
+      console.log("Password pattern is correct");
+    } else {
+      console.log("Password pattern is incorrect");
+      inputPassword.setCustomValidity("Please, enter correct password pattern\n"+
+                                      "Password length is at least 8 characters. "+
+                                      "The password uses only latin letters and numbers.")
     }
   });
 } else {
   console.log("Error with form");
-
 }
